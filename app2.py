6854b24210c844b33b65d8a29de3da5d7e88bd1e
Kevin C <42613553+TsawadaTsuna@users.noreply.github.com>
2022-10-17 11:44:25 -0500

    Update app2.py
    El que ya agarra csv

--- app2.py
+++ app2.py
@@ -11,14 +11,18 @@ from sklearn.metrics import f1_score, classification_report, confusion_matrix, r
 from sklearn.model_selection import GridSearchCV
 
 #Import Flask modules
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 #Import pickle to save our regression model
 import pickle 
 
+import os
+from os.path import join, dirname, realpath
+
 #Initialize Flask and set the template folder to "template"
 app = Flask(__name__, template_folder = 'template')
-
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 #Open our model 
 model = pickle.load(open('model.pkl','rb'))
 tfidfv = pickle.load(open('wordvector.mkl','rb'))
@@ -52,56 +56,71 @@ def predict():
     return render_template('index2.html', prediction_text = 'The review is: ${}'.format(prediction))   
 
 #Set the model
-@app.route('/', methods = ['PUT'])
+@app.route('/csv', methods = ['POST'])
 def feed():
-    dfreview = pd.read_csv(request.form.values())
+    print(request.files)
+    uploaded_file = request.files['filename']
+    if uploaded_file.filename != '':
+        try:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+            # set the file path
+            uploaded_file.save(file_path)
+            #redirect(url_for('index'))
+        
+            dfreview = pd.read_csv(uploaded_file.filename)
 
-    #slices o particiones para crear un set desbalanceado
-    dfpositivos = dfreview[dfreview['sentiment']=='positive'][:9000]
-    dfnegativos = dfreview[dfreview['sentiment']=='negative'][:1000]
-    dfreviewdes = pd.concat([dfpositivos,dfnegativos])
+            #slices o particiones para crear un set desbalanceado
+            dfpositivos = dfreview[dfreview['sentiment']=='positive'][:9000]
+            dfnegativos = dfreview[dfreview['sentiment']=='negative'][:1000]
+            dfreviewdes = pd.concat([dfpositivos,dfnegativos])
 
-    #Se hace un undersampling para balancear el dataset partido
-    rus = RandomUnderSampler()
-    dfreviewbal, dfreviewbal['sentiment'] = rus.fit_resample(dfreviewdes[['review']],dfreviewdes['sentiment'])
+            #Se hace un undersampling para balancear el dataset partido
+            rus = RandomUnderSampler()
+            dfreviewbal, dfreviewbal['sentiment'] = rus.fit_resample(dfreviewdes[['review']],dfreviewdes['sentiment'])
 
-    #se divide en un conjunto de entrenamiento y otro de prueba
-    train, test=train_test_split(dfreviewbal, test_size=0.33, random_state=42)
+            #se divide en un conjunto de entrenamiento y otro de prueba
+            train, test=train_test_split(dfreviewbal, test_size=0.33, random_state=42)
 
-    #Se llenan los conjuntos de entrenamiento y prueba en valor(review) y output(sentiment)
-    trainX, trainY=train['review'], train['sentiment']
-    testX, testY=test['review'], test['sentiment']
+            #Se llenan los conjuntos de entrenamiento y prueba en valor(review) y output(sentiment)
+            trainX, trainY=train['review'], train['sentiment']
+            testX, testY=test['review'], test['sentiment']
 
-    #ejemplo de manejo de texto a representacion numerica
-    text = ["I love writing code in Python. I love Python code",
-            "I hate writing code in Java. I hate Java code"]
-    #Ejemplo para crear una matriz de conteo de frecuencias
-    df = pd.DataFrame({'review': ['review1', 'review2'], 'text':text})
-    cv = CountVectorizer(stop_words='english')
-    cv_matrix = cv.fit_transform(df['text'])
-    df_dtm = pd.DataFrame(cv_matrix.toarray(), index=df['review'].values, columns=cv.get_feature_names_out())
+            #ejemplo de manejo de texto a representacion numerica
+            text = ["I love writing code in Python. I love Python code",
+                    "I hate writing code in Java. I hate Java code"]
+            #Ejemplo para crear una matriz de conteo de frecuencias
+            df = pd.DataFrame({'review': ['review1', 'review2'], 'text':text})
+            cv = CountVectorizer(stop_words='english')
+            cv_matrix = cv.fit_transform(df['text'])
+            df_dtm = pd.DataFrame(cv_matrix.toarray(), index=df['review'].values, columns=cv.get_feature_names_out())
 
-    #ejemplo para crear una matriz con valores tfidf
-    tfidf = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(df['text'])
-    df_dtm = pd.DataFrame(tfidf_matrix.toarray(), index=df['review'].values, columns=tfidf.get_feature_names_out())
+            #ejemplo para crear una matriz con valores tfidf
+            tfidf = TfidfVectorizer(stop_words='english')
+            tfidf_matrix = tfidf.fit_transform(df['text'])
+            df_dtm = pd.DataFrame(tfidf_matrix.toarray(), index=df['review'].values, columns=tfidf.get_feature_names_out())
 
-    #creamos y llenamos los conjuntos de valores que vamos a usar para los modelos y creamos nuestra bolsa de palabras en base al conjunto de entrenamiento
-    tfidf = TfidfVectorizer(stop_words='english')
-    trainXVector = tfidf.fit_transform(trainX)
-    testXVector = tfidf.transform(testX)
+            #creamos y llenamos los conjuntos de valores que vamos a usar para los modelos y creamos nuestra bolsa de palabras en base al conjunto de entrenamiento
+            tfidf = TfidfVectorizer(stop_words='english')
+            trainXVector = tfidf.fit_transform(trainX)
+            testXVector = tfidf.transform(testX)
 
-    #SVM
-    svc = SVC(kernel='linear')
-    svc.fit(trainXVector,trainY)
+            #SVM
+            svc = SVC(kernel='linear')
+            svc.fit(trainXVector,trainY)
 
-    #save model
-    pickle.dump(svc, open('model.pkl', 'wb'))
-    pickle.dump(tfidf, open('wordvector.mkl','wb'))
+            #save model
+            pickle.dump(svc, open('model.pkl', 'wb'))
+            pickle.dump(tfidf, open('wordvector.mkl','wb'))
 
-    model = svc
-    tfidfv = tfidf
-    return render_template('index2.html', feed_result = 'The model was feed with: ${}'.format(request.form.values())) 
+            model = svc
+            tfidfv = tfidf
+            
+            return render_template('index2.html', feed_result = 'The model was feed with: ${}'.format(uploaded_file.filename)) 
+        except Exception:
+            print(Exception)
+            return render_template('index2.html', feed_result = 'No valid file provided')
+    else:
+        return render_template('index2.html', feed_result = 'No valid file provided')
 
 #Run app
 if __name__ == "__main__":
